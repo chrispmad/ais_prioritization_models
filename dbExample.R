9e00299a6063fc1eef396413d097df85414dc970
Phelan <John.Phelan@gov.bc.ca>
2024-08-20 08:17:20 -0700

    sql example

--- dbExample.R
+++ dbExample.R
@@ -1,5 +1,6 @@
 library(DBI)
 library(RSQLite)
+library(sf)
 
 conn<-dbConnect(RSQLite::SQLite(),"../EMS/output/EMS.sqlite")
 
@@ -7,3 +8,12 @@ dbListTables(conn) # list the table(s)
 test1<-dbGetQuery(conn, "select * from results where parameter like '%Temperature%'")
 
 ##date column - collection start and collection end - if need date specific - convert back to datetime
+
+class(test1)
+str(test1)
+test1<-test1[!is.na(test1$LATITUDE),]
+test1<-test1[!is.na(test1$LONGITUDE),]
+
+tempSF<-st_as_sf(test1, coords = c("LONGITUDE","LATITUDE"), crs = 4326)
+plot(st_geometry(tempSF))
+
