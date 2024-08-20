426a71e3834244bc26926a07fa7d89f52a9b18b9
Phelan <John.Phelan@gov.bc.ca>
2024-08-20 13:28:32 -0700

    remove things that are not example

--- dbExample.R
+++ dbExample.R
@@ -1,6 +1,8 @@
 library(DBI)
 library(RSQLite)
 library(sf)
+library(ggplot2)
+library(dplyr)
 
 conn<-dbConnect(RSQLite::SQLite(),"../EMS/output/EMS.sqlite")
 
@@ -8,12 +10,3 @@ dbListTables(conn) # list the table(s)
 test1<-dbGetQuery(conn, "select * from results where parameter like '%Temperature%'")
 
 ##date column - collection start and collection end - if need date specific - convert back to datetime
-
-class(test1)
-str(test1)
-test1<-test1[!is.na(test1$LATITUDE),]
-test1<-test1[!is.na(test1$LONGITUDE),]
-
-tempSF<-st_as_sf(test1, coords = c("LONGITUDE","LATITUDE"), crs = 4326)
-plot(st_geometry(tempSF))
-
