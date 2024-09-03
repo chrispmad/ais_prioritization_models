e26ba040066b44b40bd60d4eb908db6a7840fae8
Phelan <John.Phelan@gov.bc.ca>
2024-09-03 16:41:10 -0700

    krig ems

---
+++ scripts/utils/krig_ems.R
@@ -0,0 +1,127 @@
+library(DBI)
+library(RSQLite)
+library(sf)
+library(ggplot2)
+library(dplyr)
+library(bcmaps)
+library(raster)
+library(automap)
+library(gstat)
+library(stringr)
+library(terra)
+library(raster)
+library(lubridate)
+
+krig_ems<-function(var_name){
+  
+  bc = bcmaps::bc_bound() |> 
+    dplyr::summarise() |> 
+    terra::vect()
+  
+  path_to_user = str_extract(getwd(), ".*Users/[A-Z]+")
+  onedrive_path = paste0(path_to_user,"/OneDrive - Government of BC/data/CNF/")
+  
+  bc_vect = terra::vect(sf::st_transform(bcmaps::bc_bound(),4326))
+  bc_vect_alb = terra::vect(sf::st_transform(bcmaps::bc_bound(),3005))
+  
+  conn<-dbConnect(RSQLite::SQLite(),"../EMS/output/EMS.sqlite")
+  
+  test1<-dbGetQuery(conn, paste0("select * from results where parameter like '%",var_name,"%'"))
+  
+  dbDisconnect(conn)
+  
+  test1<-test1[!is.na(test1$LATITUDE),]
+  test1<-test1[!is.na(test1$LONGITUDE),]
+  
+  tempSF<-st_as_sf(test1, coords = c("LONGITUDE","LATITUDE"), crs = 4326)
+  tempSF$COLLECTION_DATE<-as.Date(tempSF$COLLECTION_DATE)
+  
+  results<-tempSF %>%
+    dplyr::select(c(RESULT,COLLECTION_DATE,LOCATION_TYPE,LOCATION_PURPOSE,MONITORING_LOCATION, geometry)) %>% 
+    dplyr::filter(!is.na(RESULT)) %>% 
+    dplyr::filter(LOCATION_TYPE == "RIVER,STREAM OR CREEK" |
+                    LOCATION_TYPE == "MONITORING WELL" |
+                    LOCATION_TYPE == "LAKE OR POND")
+  results_albers = sf::st_transform(results, 3005)
+  
+  # Simplify our input data so that there is one point of data per 100km^2 raster cell.
+  interp_grid = sf::st_make_grid(sf::st_as_sf(bc_vect_alb), cellsize = c(10000,10000)) |> 
+    sf::st_as_sf() |> 
+    sf::st_filter(sf::st_as_sf(bc_vect_alb))
+  
+  results_albers_overlap = interp_grid |> 
+    dplyr::mutate(row_id = row_number()) |> 
+    sf::st_join(results_albers)
+  
+  results_albers_overlap = results_albers_overlap |> 
+    dplyr::group_by(row_id) |> 
+    dplyr::mutate(medianVal = median(RESULT)) |> 
+    dplyr::ungroup() |> 
+    dplyr::filter(!duplicated(row_id))
+  
+  results_albers_as_centroids = results_albers_overlap |> 
+    sf::st_centroid()
+  
+  ggplot() + geom_sf(data = results_albers_as_centroids, aes(fill = medianVal, col = medianVal))
+  
+  results_albers_as_centroids_no_na = results_albers_as_centroids |> dplyr::filter(!is.na(medianVal))
+  
+  toInterp<-st_transform(results_albers_as_centroids_no_na, 3005)
+  
+  tointerpPt<- toInterp %>% 
+    dplyr::mutate(lon = sf::st_coordinates(.)[,1],
+                  lat = sf::st_coordinates(.)[,2])
+  
+  #create interpolation grid encompassing Canada and USA
+  bbox <- sf::st_bbox(st_as_sf(bc))
+  
+  
+  # Bring in raster for reference (dimensions, extent, etc.)
+  pred_bioc = terra::rast("CMI/climate/wc2.1_5m/wc2.1_5m_bioc_ACCESS-CM2_ssp585_2021-2040.tif")
+  bc_vect = terra::vect(sf::st_transform(bcmaps::bc_bound(),4326))
+  pred_bioc_clipped = mask(crop(pred_bioc, bc_vect), bc_vect)
+  
+  #terra::plot(pred_bioc_clipped$bio01)
+  
+  ref = pred_bioc_clipped$bio01
+  
+  ref <- terra::project(ref, "EPSG:3005")
+  values(ref)<-1
+  
+  grid10km <- expand.grid(
+    height = ) %>%
+    mutate(Z = 0)  %>%
+    raster::rasterFromXYZ(crs = 3005)
+  
+  st_crs(results_albers_as_centroids_no_na)
+  st_crs(grid10km)
+  
+  varKRVar <- autofitVariogram(medianVal ~ 1, 
+                               as(results_albers_as_centroids_no_na, "Spatial"),
+                               verbose=TRUE,
+                               fix.values = c(0,NA,NA))
+  
+  KRvarmod <- gstat(formula=medianVal~1,
+                    locations=as(results_albers_as_centroids_no_na,"Spatial"),
+                    model=varKRVar$var_model
+  )
+  
+  # KRgrid10km <- as(ref, "SpatialGrid")
+  KRVar_interpolation <- predict(KRvarmod, raster(ref), debug.level = -1)
+  
+  interp_r = terra::rast(KRVar_interpolation)
+  interp_r = terra::mask(interp_r, bc_vect_alb)
+  
+  KRVar_interpolation_raster <- raster(KRVar_interpolation) 
+  plot(KRVar_interpolation_raster)
+  
+  spatRast<-rast(KRVar_interpolation_raster)
+  
+  KRVar_interpolation_vairance_raster<-raster(KRVar_interpolation, layer = "var1.var")
+  spatRastVar<-rast(KRVar_interpolation_vairance_raster)
+  var_save<-gsub(" ", "_", var_name)
+  new_path <- gsub("CNF/", "", onedrive_path)
+  terra::writeRaster(spatRast, paste0(new_path,"raster/",var_save,"_",year_to_search,"_krig.tif"), overwrite = T)
+  terra::writeRaster(spatRastVar, paste0(new_path,"raster/",var_save,"_",year_to_search,"_var_krig.tif"), overwrite = T)
+  
+}
